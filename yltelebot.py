509ff83683730e07ab5f7fce4fc9658ba273e226
siahyeelong <100999280+siahyeelong@users.noreply.github.com>
2022-03-06 13:34:21 +0800

    Add files via upload

---
+++ yltelebot.py
@@ -0,0 +1,27 @@
+from telegram.ext import Updater, Dispatcher
+from telegram.ext import CommandHandler, MessageHandler
+import os
+
+TOKEN = os.environ.get("TELEGRAMID")
+
+def start(update, context):
+    yourname = update.message.chat.first_name
+    msg = "Hi" + yourname +"!"
+    context.bot.send_message(update.message.chat.id, msg)
+
+def error(update, context):
+    context.bot.send_message(update.message.chat.id, "Command not recognised")
+
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
+
+    dp = updater.Dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_error_handler(error)
+
+    updater.start_webhook(listen="0.0.0.0", port=os.environ.get("PORT", 443), url_path=TOKEN, webhook_url="https://yltestbench.heroku.com/"+TOKEN)
+    updater.idle()
+
+if __name__=='__main__':
+    main()
