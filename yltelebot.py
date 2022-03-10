d9a2a4eee603da79190c5cdb20544e3a92c32c01
siahyeelong <100999280+siahyeelong@users.noreply.github.com>
2022-03-10 12:40:12 +0800

    Update yltelebot.py

--- yltelebot.py
+++ yltelebot.py
@@ -6,7 +6,11 @@ TOKEN = os.environ['TELEGRAMID']
 
 def start(update, context):
     yourname = update.message.chat.first_name
-    msg = "Hi" + yourname +"!"
+    msg = "Hi " + yourname +"! Welcome to YL's test bot. To begin, type /help to see what you can do"
+    context.bot.send_message(update.message.chat.id, msg)
+    
+def help(update, context):
+    msg = "(work in progress)"
     context.bot.send_message(update.message.chat.id, msg)
 
 def error(update, context):
@@ -18,6 +22,7 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
     dp.add_error_handler(error)
 
     updater.start_webhook(listen="0.0.0.0", port=os.environ.get("PORT", 8443), url_path=TOKEN, webhook_url="https://yltestbot.herokuapp.com/"+TOKEN)
