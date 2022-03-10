c6a1e285e5dc8ade0b3d8b76b42bf50ec1096e46
siahyeelong <100999280+siahyeelong@users.noreply.github.com>
2022-03-10 09:27:28 +0800

    Update yltelebot.py

--- yltelebot.py
+++ yltelebot.py
@@ -2,7 +2,7 @@ from telegram.ext import Updater, Dispatcher
 from telegram.ext import CommandHandler, MessageHandler
 import os
 
-TOKEN = os.environ.get("TELEGRAMID")
+TOKEN = os.environ['TELEGRAMID']
 
 def start(update, context):
     yourname = update.message.chat.first_name
@@ -13,14 +13,14 @@ def error(update, context):
     context.bot.send_message(update.message.chat.id, "Command not recognised")
 
 def main():
-    updater = Updater(token=TOKEN, use_context=True)
+    updater = Updater(TOKEN, use_context=True)
 
-    dp = updater.Dispatcher
+    dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_error_handler(error)
 
-    updater.start_webhook(listen="0.0.0.0", port=os.environ.get("PORT", 443), url_path=TOKEN, webhook_url="https://yltestbench.heroku.com/"+TOKEN)
+    updater.start_webhook(listen="0.0.0.0", port=os.environ.get("PORT", 8443), url_path=TOKEN, webhook_url="https://yltestbench.herokuapp.com/"+TOKEN)
     updater.idle()
 
 if __name__=='__main__':
